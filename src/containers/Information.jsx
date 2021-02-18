bef0730f828e6f9711ef2387c4ee915f62ddbd13
Nicolas Posa <nicoposa57@gmail.com>
2021-02-18 16:52:05 -0300

    integrando iconos

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/components/Information.css'
 
 export default function Information() {
@@ -26,7 +27,9 @@ export default function Information() {
             Regresar
           </div>
           <div className="Information-next">
-            Pagar
+            <Link to="/checkout/payment">
+              Pagar
+            </Link>
           </div>
         </div>
       </div>
